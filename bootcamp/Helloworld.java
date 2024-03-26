7c3b25466d5f8c831e03a290d880391f2009acba
fatecharles <fatecharles@icloud.com>
2024-03-26 10:30:02 +0800

    update Helloworld

---
+++ bootcamp/Helloworld.java
@@ -0,0 +1,9 @@
+public class Helloworld{
+  public static void main(String[] args){
+    // Start point of the program 你好
+    // Tool : System.out.println()
+    System.out.println("Hello, testing testing...");
+    System.out.println("Hello, This is Charles Java. Welcome to bootcamp!");
+    
+  }
+}
