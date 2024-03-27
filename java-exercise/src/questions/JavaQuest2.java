113847bbd95e621de5cfc46d480791f75786c277
fatecharles <fatecharles@icloud.com>
2024-03-27 17:21:37 +0800

    update

--- java-exercise/src/questions/JavaQuest2.java
+++ java-exercise/src/questions/JavaQuest2.java
@@ -1,11 +1,9 @@
 package questions;
+
 /**
  * Expected Output:
  * 
- * a > 9
- * str length < 27
- * 2020 is a leap year.
- * 2400 is a leap year.
+ * a > 9 str length < 27 2020 is a leap year. 2400 is a leap year.
  */
 // Primitive types and operators
 public class JavaQuest2 {
@@ -17,11 +15,11 @@ public class JavaQuest2 {
     } else if (a < 9 && a >= 0) {
       System.out.println("a is between 0 and 8");
       // code here ...
-    } else if  (a >= 10) {
-        System.out.println("a > 9");
-    }else {
+    } else if (a >= 10) {
+      System.out.println("a > 9");
+    } else {
       System.out.println("a < 0");
-}
+    }
 
 
     String str = "I love Java. I love coding";
@@ -35,9 +33,7 @@ public class JavaQuest2 {
 
     // Question : Determine whether year n is a leap year.
     /*
-     * Definiton of a leap year:
-     * If a year is divisible by 4 and not divisible by 100 or divisible by 400,
-     * then, it is a leap year.
+     * Definiton of a leap year: If a year is divisible by 4 and not divisible by 100 or divisible by 400, then, it is a leap year.
      */
     int year1 = 2009;
     // Program a condition to check if it is a leap year.
@@ -56,7 +52,7 @@ public class JavaQuest2 {
     int year3 = 2400;
     if (year3 % 4 == 0 && !(year3 % 100 == 0) || year3 % 400 == 0) {
       System.out.println(year3 + " is a leap year.");
-    };
+    } ;
     int year5 = 2100;
     // code here ...same as the above, but it will NOT print out.
     if (year5 % 4 == 0 && !(year5 % 100 == 0) || year5 % 400 == 0) {
